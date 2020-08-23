203932e551a3b80c5f0c48f51cd35402a7bfe08d
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-08-23 14:58:30 +0300

    FPO

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Unistall SEO Ready.
+ * Fired when the plugin is uninstalled.
+ *
+ * @author      Mahdi Yazdani
+ * @package     SEO Ready
+ * @since       1.0.0
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
