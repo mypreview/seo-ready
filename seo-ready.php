ef3b0a98f60499a0a9d0f35afc1d788eddf2f936
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-08-24 00:20:11 +0300

    Print out meta tag template used for search engine optimization

--- seo-ready.php
+++ seo-ready.php
@@ -45,14 +45,17 @@ if ( ! defined( 'WPINC' ) ) {
 $seo_ready_plugin_data = get_file_data(
 	__FILE__,
 	array(
+		'name'       => 'Plugin Name',
 		'author_uri' => 'Author URI',
 		'version'    => 'Version',
 	),
 	'plugin'
 );
+define( 'SEO_READY_NAME', $seo_ready_plugin_data['name'] );
 define( 'SEO_READY_VERSION', $seo_ready_plugin_data['version'] );
 define( 'SEO_READY_AUTHOR_URI', $seo_ready_plugin_data['author_uri'] );
 define( 'SEO_READY_SLUG', 'seo-ready' );
+define( 'SEO_READY_PREFIX', 'seo_ready' );
 define( 'SEO_READY_FILE', __FILE__ );
 define( 'SEO_READY_BASENAME', basename( SEO_READY_FILE ) );
 define( 'SEO_READY_PLUGIN_BASENAME', plugin_basename( SEO_READY_FILE ) );
@@ -99,6 +102,8 @@ if ( ! class_exists( 'SEO_Ready' ) ) :
 		protected function __construct() {
 			add_action( 'init', array( $this, 'textdomain' ) );
 			add_action( 'init', array( $this, 'register_meta' ) );
+			add_action( 'wp_head', array( $this, 'print_tags' ) );
+			add_filter( 'pre_get_document_title', array( $this, 'overwrite_title' ) );
 			add_action( 'enqueue_block_editor_assets', array( $this, 'editor_enqueue' ) );
 			add_filter( sprintf( 'plugin_action_links_%s', SEO_READY_PLUGIN_BASENAME ), array( $this, 'additional_links' ) );
 		}
@@ -148,15 +153,15 @@ if ( ! class_exists( 'SEO_Ready' ) ) :
 		public function register_meta() {
 			register_post_meta(
 				'post',
-				'seo_ready',
+				SEO_READY_PREFIX,
 				array(
 					'single'        => true,
 					'type'          => 'object',
 					'show_in_rest'  => array(
 						'schema' => array(
-							'type'       => 'object',
+							'type'                 => 'object',
 							'additionalProperties' => false,
-							'properties' => array(
+							'properties'           => array(
 								'title' => array(
 									'type' => 'string',
 								),
@@ -186,6 +191,80 @@ if ( ! class_exists( 'SEO_Ready' ) ) :
 		}
 
 		/**
+		 * Meta tag template used for search engine optimization.
+		 *
+		 * @access   public
+		 * @since    1.0.0
+		 * @return   void
+		 */
+		public function print_tags() {
+			// Retrieve ID of the current queried object.
+			$metas = self::has_tags();
+
+			if ( is_array( $metas ) ) {
+				$output = '';
+				// Keywords.
+				$keywords = isset( $metas['keywords'] ) ? $metas['keywords'] : null;
+				if ( ! is_null( $keywords ) ) {
+					$output .= sprintf( '<meta name="keywords" content="%s" />%s', esc_html( $keywords ), PHP_EOL );
+				}
+				// Description.
+				$description = isset( $metas['description'] ) ? $metas['description'] : null;
+				if ( ! is_null( $description ) ) {
+					$output .= sprintf( '<meta name="description" content="%s" />%s', esc_html( $description ), PHP_EOL );
+				}
+				// Canonical.
+				$canonical = isset( $metas['canonical'] ) ? $metas['canonical'] : null;
+				if ( ! is_null( $canonical ) ) {
+					$output .= sprintf( '<meta name="canonical" href="%s" />%s', esc_url( $canonical ), PHP_EOL );
+				}
+				// No-Index.
+				$noindex = isset( $metas['noindex'] ) ? $metas['noindex'] : false;
+				if ( ! ! $noindex ) {
+					$output .= sprintf( '<meta name="robots" content="noindex" />%s', PHP_EOL );
+				}
+				// No-Follow.
+				$nofollow = isset( $metas['nofollow'] ) ? $metas['nofollow'] : false;
+				if ( ! ! $nofollow ) {
+					$output .= sprintf( '<meta name="robots" content="nofollow" />%s', PHP_EOL );
+				}
+
+				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped, WordPress.NamingConventions.ValidHookName.UseUnderscores, WordPress.NamingConventions.PrefixAllGlobals.DynamicHooknameFound
+				echo apply_filters( sprintf( '%s_print_tags', SEO_READY_PREFIX ), $output );
+			}
+		}
+
+		/**
+		 * Overrides the document title before it is generated.
+		 *
+		 * @access   public
+		 * @since    1.0.0
+		 * @param    string $document_title      The document title.
+		 * @return   string
+		 */
+		public function overwrite_title( $document_title ) {
+			// Retrieve ID of the current queried object.
+			$metas = self::has_tags();
+
+			if ( is_array( $metas ) ) {
+				// Title.
+				$title = isset( $metas['title'] ) ? $metas['title'] : null;
+				if ( ! is_null( $title ) ) {
+					// Replaces common plain text characters with formatted entities.
+					$document_title = wptexturize( $title );
+					// Converts lone & characters into &#038; (a.k.a. &amp;).
+					$document_title = convert_chars( $document_title );
+					// Escaping for HTML parts.
+					$document_title = esc_html( $document_title );
+					// Forever eliminate “WordPress” from the planet!!
+					$document_title = capital_P_dangit( $document_title );
+				}
+			}
+
+			return $document_title;
+		}
+
+		/**
 		 * Register the stylesheets and JavaScript for the Gutenberg (editor) side of the site.
 		 *
 		 * @access   public
@@ -212,7 +291,7 @@ if ( ! class_exists( 'SEO_Ready' ) ) :
 		 * @access   public
 		 * @since    1.0.0
 		 * @param    array $links      An array of plugin action links.
-		 * @return   array   $links
+		 * @return   array
 		 */
 		public function additional_links( $links ) {
 			$plugin_links = array();
@@ -222,6 +301,29 @@ if ( ! class_exists( 'SEO_Ready' ) ) :
 			$plugin_links[] = sprintf( _x( '%1$sSupport%2$s', 'plugin link', 'seo-ready' ), sprintf( '<a href="https://wordpress.org/support/plugin/%s" target="_blank" rel="noopener noreferrer nofollow">', SEO_READY_SLUG ), '</a>' );
 
 			return array_merge( $plugin_links, $links );
+		}
+
+		/**
+		 * Whether there are any SEO meta tags to print-out?
+		 * If so, then return a list of available tags/values.
+		 *
+		 * @access   public
+		 * @since    1.0.0
+		 * @return   bool|array
+		 */
+		public static function has_tags() {
+			// Retrieve ID of the current queried object.
+			$object_id = (int) get_queried_object_id();
+
+			if ( $object_id ) {
+				// Retrieves the SEO post meta field for the queried post ID.
+				$metas = get_post_meta( $object_id, SEO_READY_PREFIX, true );
+				if ( is_array( $metas ) ) {
+					return $metas;
+				}
+			}
+
+			return false;
 		}
 
 	}
