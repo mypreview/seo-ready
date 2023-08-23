e88e6ab94c998f193959f5e30cbe197f71aef24e
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-08-24 00:04:50 +0300

    fix: Bootstrap the plugin

--- seo-ready.php
+++ seo-ready.php
@@ -2,236 +2,173 @@
 /**
  * The `SEO Ready` bootstrap file.
  *
- * The plugin bootstrap file.
  * This file is read by WordPress to generate the plugin information in the plugin
  * admin area. This file also includes all of the dependencies used by the plugin,
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * SEO Ready is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * You can redistribute this plugin/software and/or modify it under
+ * the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * @link                https://www.mypreview.one
- * @since               2.0.0
- * @package             seo-ready
- * @author              MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
- * @copyright           © 2015 - 2022 MyPreview. All Rights Reserved.
+ * @link https://www.mypreview.one
+ *
+ * @since 1.0.0
+ *
+ * @package seo-ready
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @copyright © 2015 - 2023 MyPreview. All Rights Reserved.
  *
  * @wordpress-plugin
- * Plugin Name:         SEO Ready
- * Plugin URI:          https://www.mypreview.one
- * Description:         This plugin offers a GUI to generate most commonly used meta tags, so you can optimize your WordPress site for search engines… and do it in less time than it takes to brew a cup of coffee.
- * Version:             2.0.1
- * Requires at least:   5.5
- * Requires PHP:        7.4
- * Author:              MyPreview
- * Author URI:          https://mahdiyazdani.com
- * License:             GPL-3.0+
- * License URI:         http://www.gnu.org/licenses/gpl-3.0.txt
- * Text Domain:         seo-ready
- * Domain Path:         /languages
+ * Plugin Name: SEO Ready
+ * Plugin URI: https://mypreview.one
+ * Description: A lightweight SEO plugin to generate most commonly used meta tags. Designed for privacy, speed, and accessibility.
+ * Version: 2.1.0
+ * Author: MyPreview
+ * Author URI: https://mypreview.one
+ * Requires at least: 5.9
+ * Requires PHP: 7.4
+ * License: GPL-3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain: seo-ready
+ * Domain Path: /languages
  */
 
-namespace Seo_Ready;
-
-use function Seo_Ready\Includes\Utils\has_tags as has_tags;
-use function Seo_Ready\Includes\Utils\get_localized_post_id as get_localized_post_id;
-
-define(
-	__NAMESPACE__ . '\PLUGIN',
-	array(
-		'basename' => plugin_basename( __FILE__ ),
-		'dir_path' => untrailingslashit( plugin_dir_path( __FILE__ ) ),
-		'dir_url'  => untrailingslashit( plugin_dir_url( __FILE__ ) ),
-		'slug'     => 'seo-ready',
-	)
-);
-
-require_once PLUGIN['dir_path'] . '/includes/utils.php';
-
-/**
- * Load the plugin text domain for translation.
- *
- * @since     2.0.0
- * @return    void
- */
-function textdomain(): void {
-	load_plugin_textdomain( 'seo-ready', false, dirname( PLUGIN['basename'] ) . '/languages' );
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
 }
-add_action( 'init', __NAMESPACE__ . '\textdomain' );
 
 /**
  * Registers custom meta keys for a specific combination of object type and object subtype.
  *
- * @since     2.0.0
- * @return    void
+ * @since 2.0.1
+ * @
+ * @return void
  */
-function register_meta(): void {
-	register_post_meta(
-		'',
-		'seo_ready',
-		array(
-			'single'        => true,
-			'type'          => 'object',
-			'show_in_rest'  => array(
-				'schema' => array(
-					'type'                 => 'object',
-					'additionalProperties' => false,
-					'properties'           => array(
-						'title' => array(
-							'sanitize_callback' => 'sanitize_text_field',
-							'type'              => 'string',
-						),
-						'keywords' => array(
-							'sanitize_callback' => 'sanitize_text_field',
-							'type'              => 'string',
-						),
-						'description' => array(
-							'sanitize_callback' => 'sanitize_text_field',
-							'type'              => 'string',
-						),
-						'canonical' => array(
-							'sanitize_callback' => 'sanitize_text_field',
-							'type'              => 'string',
-						),
-						'noindex' => array(
-							'sanitize_callback' => 'rest_sanitize_boolean',
-							'type'              => 'boolean',
-						),
-						'nofollow' => array(
-							'sanitize_callback' => 'rest_sanitize_boolean',
-							'type'              => 'boolean',
-						),
-					),
-				),
-			),
-			'auth_callback' => function() {
-				return current_user_can( 'edit_posts' );
-			},
-		)
-	);
-}
-add_action( 'init', __NAMESPACE__ . '\register_meta' );
+function seo_ready_register_meta() {
 
-/**
- * Registers custom meta keys for a specific combination of object type and object subtype.
- *
- * @since     2.0.0
- * @return    void
- */
-function print_tags(): void {
-	// Retrieve ID of the current queried object.
-	$meta = has_tags();
+	$allowed_post_types = apply_filters( 'seo_ready_allowed_post_types', array( 'post', 'page' ) );
 
-	if ( empty( $meta ) ) {
+	// Bail early if no post types are allowed.
+	if ( empty( $allowed_post_types ) ) {
 		return;
 	}
 
-	$print = '';
-	// Keywords.
-	$keywords = $meta['keywords'] ?? null;
-	if ( ! is_null( $keywords ) ) {
-		$print .= '<meta name="keywords" content="' . esc_html( $keywords ) . '" />' . PHP_EOL;
-	}
-	// Description.
-	$description = $meta['description'] ?? null;
-	if ( ! is_null( $description ) ) {
-		$print .= '<meta name="description" content="' . esc_html( $description ) . '" />' . PHP_EOL;
-	}
-	// Canonical.
-	$canonical = $meta['canonical'] ?? null;
-	if ( ! is_null( $canonical ) ) {
-		$print .= '<meta name="canonical" href="' . esc_url( $canonical ) . '" />' . PHP_EOL;
-	}
-	// No-Index.
-	$noindex = $meta['noindex'] ?? false;
-	if ( ! ! $noindex ) {
-		$print .= '<meta name="robots" content="noindex" />' . PHP_EOL;
-	}
-	// No-Follow.
-	$nofollow = $meta['nofollow'] ?? false;
-	if ( ! ! $nofollow ) {
-		$print .= '<meta name="robots" content="nofollow" />' . PHP_EOL;
-	}
+	foreach ( $allowed_post_types as $post_type ) {
 
-	// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	echo apply_filters( 'seo_ready_wp_head_print_tags', $print, get_localized_post_id() );
+		register_post_meta(
+			$post_type,
+			'seo_ready',
+			array(
+				'single'        => true,
+				'type'          => 'object',
+				'show_in_rest'  => array(
+					'schema' => array(
+						'type'                 => 'object',
+						'additionalProperties' => false,
+						'properties'           => seo_ready_get_properties(),
+					),
+				),
+				'auth_callback' => function() {
+					return current_user_can( 'edit_posts' );
+				},
+			)
+		);
+	}
 }
-add_action( 'wp_head', __NAMESPACE__ . '\print_tags' );
+add_action( 'init', 'seo_ready_register_meta' );
 
 /**
- * Overrides the document title before it is generated.
+ * Enqueue scripts and styles for the editor.
  *
- * @since     2.0.0
- * @return    string
- * @phpcs:disable WordPress.WP.CapitalPDangit.Misspelled
+ * @since 2.1.0
+ * @
+ * @return void
  */
-function overwrite_title(): ?string {
-	$return = null;
-	$meta   = has_tags();
+function seo_ready_enqueue_editor() {
 
-	if ( empty( $meta ) ) {
-		return $return;
-	}
+	$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : trailingslashit( 'minified' );
 
-	// Title.
-	$title = $meta['title'] ?? null;
-	if ( ! is_null( $title ) ) {
-		// Replaces common plain text characters with formatted entities.
-		$return = wptexturize( $title );
-		// Converts lone & characters into &#038; (a.k.a. &amp;).
-		$return = convert_chars( $return );
-		// Escaping for HTML parts.
-		$return = esc_html( $return );
-		// Forever eliminate “Wordpress” from the planet!!
-		// Or at least the little bit we can influence.
-		$return = capital_P_dangit( $return );
-	}
-
-	return $return;
-}
-add_filter( 'pre_get_document_title', __NAMESPACE__ . '\overwrite_title' );
-
-/**
- * Enqueue the plugin script after block assets have been enqueued for the editing interface.
- *
- * @since     2.0.0
- * @return    void
- */
-function enqueue(): void {
-	$file_path       = PLUGIN['dir_path'] . '/build/index.js';
-	$file_asset_path = PLUGIN['dir_path'] . '/build/index.asset.php';
-	$asset           = file_exists( $file_asset_path ) ? require $file_asset_path : array(
-		'dependencies' => $dependencies,
-		'version'      => filemtime( $file_path ),
+	wp_enqueue_script(
+		'seo-ready-editor',
+		untrailingslashit( plugin_dir_url( __FILE__ ) ) . "/assets/js/{$min}plugin.js",
+		array( 'lodash', 'react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives' ),
+		'2.1.0',
+		true
 	);
-
-	wp_enqueue_script( PLUGIN['slug'], PLUGIN['dir_url'] . '/build/index.js', $asset['dependencies'] ?? array(), $asset['version'] ?? '1.0', true );
-	wp_set_script_translations( PLUGIN['slug'], 'seo-ready', PLUGIN['dir_path'] . '/languages/' );
 }
-add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue' );
+add_action( 'enqueue_block_editor_assets', 'seo_ready_enqueue_editor' );
 
 /**
- * Add additional helpful links to the plugin’s metadata.
+ * Registers custom meta keys for a specific combination of object type and object subtype.
  *
- * @since     2.0.0
- * @param     array  $links    An array of the plugin’s metadata.
- * @param     string $file     Path to the plugin file relative to the plugins directory.
- * @return    array
+ * @since 2.1.0
+ * @
+ * @return array
  */
-function add_meta_links( array $links, string $file ): array {
-	if ( PLUGIN['basename'] !== $file ) {
-		return $links;
-	}
+function seo_ready_get_properties() {
 
-	$plugin_links = array();
-	/* translators: 1: Open anchor tag, 2: Close anchor tag. */
-	$plugin_links[] = sprintf( _x( '%1$sCommunity support%2$s', 'plugin link', 'seo-ready' ), sprintf( '<a href="https://wordpress.org/support/plugin/%s" target="_blank" rel="noopener noreferrer nofollow">', PLUGIN['slug'] ), '</a>' );
-	/* translators: 1: Open anchor tag, 2: Close anchor tag. */
-	$plugin_links[] = sprintf( _x( '%1$sDonate%2$s', 'plugin link', 'seo-ready' ), sprintf( '<a href="https://www.buymeacoffee.com/mahdiyazdani" class="button-link-delete" target="_blank" rel="noopener noreferrer nofollow" title="%s">☕ ', esc_attr__( 'Donate to support this plugin', 'seo-ready' ) ), '</a>' );
-
-	return array_merge( $links, $plugin_links );
+	return array(
+		'title' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'keywords' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'description' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'canonical' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'noindex' => array(
+			'sanitize_callback' => 'rest_sanitize_boolean',
+			'type'              => 'boolean',
+		),
+		'nofollow' => array(
+			'sanitize_callback' => 'rest_sanitize_boolean',
+			'type'              => 'boolean',
+		),
+		'schema_type' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'schema_article_type' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'facebook_image' => array(
+			'sanitize_callback' => 'absint',
+			'type'              => 'integer',
+		),
+		'facebook_title' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'facebook_description' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'twitter_image' => array(
+			'sanitize_callback' => 'absint',
+			'type'              => 'integer',
+		),
+		'twitter_title' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+		'twitter_description' => array(
+			'sanitize_callback' => 'sanitize_text_field',
+			'type'              => 'string',
+		),
+	);
 }
-add_filter( 'plugin_row_meta', __NAMESPACE__ . '\add_meta_links', 10, 2 );
